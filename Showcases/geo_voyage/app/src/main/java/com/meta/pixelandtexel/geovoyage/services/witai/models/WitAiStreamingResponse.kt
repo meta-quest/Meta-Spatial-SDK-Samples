9acd2faf45fd45a699c89822155c92d4bfc99a1f
Spencer E <spencer@pixelandtexel.com>
2024-11-01 15:47:30 -0400

    added error handling
    - added error handling for wit ai 200 responses
    - gitignore update
    - version bump

--- Showcases/geo_voyage/app/src/main/java/com/meta/pixelandtexel/geovoyage/services/witai/models/WitAiStreamingResponse.kt
+++ Showcases/geo_voyage/app/src/main/java/com/meta/pixelandtexel/geovoyage/services/witai/models/WitAiStreamingResponse.kt
@@ -6,8 +6,11 @@ import com.meta.pixelandtexel.geovoyage.services.witai.enums.WitAiResponseType
 
 /*
 The response from WIT ai as it streams chunks.
+https://wit.ai/docs/http/20240304/
  */
 data class WitAiStreamingResponse(
     val text: String,
     val type: WitAiResponseType,
+    val error: String?,
+    val code: String?,
 )
