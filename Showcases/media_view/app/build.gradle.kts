7a102e8eb85aca7d5e1fd6d10b738050a9a809a7
JTorrus <jtorrusgalindo@gmail.com>
2024-12-02 13:11:31 +0100

    fix(media_view): rollback to previous AndroidX and ExoPlayer versions due to target SDK incompatibility issues

--- Showcases/media_view/app/build.gradle.kts
+++ Showcases/media_view/app/build.gradle.kts
@@ -7,13 +7,13 @@ import java.util.Properties
 val metaSpatialSdkVersion: String by project
 
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
-  id("kotlin-kapt")
-  id("com.google.dagger.hilt.android")
-  id("kotlin-parcelize")
-  id("com.meta.spatial.plugin")
-  id("com.datadoghq.dd-sdk-android-gradle-plugin")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
+    id("com.meta.spatial.plugin")
+    id("com.datadoghq.dd-sdk-android-gradle-plugin")
 }
 
 // Signing
@@ -21,102 +21,124 @@ val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("private/keystore.properties")
 
 if (keystorePropertiesFile.exists()) {
-  keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-  project.logger.info("Signing loaded from local file")
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    project.logger.info("Signing loaded from local file")
 } else if (System.getenv("KEYSTORE_FILE") != null) {
-  keystoreProperties.setProperty("keystoreFile", System.getenv("KEYSTORE_FILE"))
-  keystoreProperties.setProperty("keystorePassword", System.getenv("KEYSTORE_PASSWORD"))
-  keystoreProperties.setProperty("keyAlias", System.getenv("KEY_ALIAS"))
-  keystoreProperties.setProperty("keyPassword", System.getenv("KEY_PASSWORD"))
+    keystoreProperties.setProperty("keystoreFile", System.getenv("KEYSTORE_FILE"))
+    keystoreProperties.setProperty("keystorePassword", System.getenv("KEYSTORE_PASSWORD"))
+    keystoreProperties.setProperty("keyAlias", System.getenv("KEY_ALIAS"))
+    keystoreProperties.setProperty("keyPassword", System.getenv("KEY_PASSWORD"))
 } else {
-  project.logger.info("No release signing available")
+    project.logger.info("No release signing available")
 }
 
 android {
-  namespace = "com.meta.levinriegner.mediaview"
-  compileSdk = 34
+    namespace = "com.meta.levinriegner.mediaview"
+    compileSdk = 34
 
-  defaultConfig {
-    applicationId = "com.meta.levinriegner.mediaview"
-    minSdk = 29
-    //noinspection ExpiredTargetSdkVersion
-    targetSdk = 32
-    versionCode = 14
-    versionName = "0.0.13"
+    defaultConfig {
+        applicationId = "com.meta.levinriegner.mediaview"
+        minSdk = 29
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 32
+        versionCode = 14
+        versionName = "0.0.13"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    vectorDrawables { useSupportLibrary = true }
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables { useSupportLibrary = true }
 
-    // API Keys
-    val properties = Properties()
-    properties.load(project.rootProject.file("local.properties").inputStream())
-    // Google Drive Keys
-    buildConfigField(
-        "String",
-        "DRIVE_CLIENT_ID",
-        System.getenv("DRIVE_CLIENT_ID")
-            ?: properties.getProperty("DRIVE_CLIENT_ID")
-            ?: "\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\"")
-    buildConfigField(
-        "String",
-        "DRIVE_API_KEY",
-        System.getenv("DRIVE_API_KEY")
-            ?: properties.getProperty("DRIVE_API_KEY")
-            ?: "\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\"")
-    buildConfigField(
-        "String",
-        "DRIVE_APP_ID",
-        System.getenv("DRIVE_APP_ID")
-            ?: properties.getProperty("DRIVE_APP_ID")
-            ?: "\"XXXXXXXXXXXX\"")
-    // Datadog Keys
-    buildConfigField(
-      "String",
-      "DATADOG_CLIENT_TOKEN",
-      System.getenv("DATADOG_CLIENT_TOKEN")
-        ?: properties.getProperty("DATADOG_CLIENT_TOKEN")
-        ?: "\"XXXXXXXXXXXX\"")
-    buildConfigField(
-      "String",
-      "DATADOG_APPLICATION_ID",
-      System.getenv("DATADOG_APPLICATION_ID")
-        ?: properties.getProperty("DATADOG_APPLICATION_ID")
-        ?: "\"XXXXXXXXXXXX\"")
-  }
-
-  buildTypes {
-    debug {
-      isMinifyEnabled = false
-      isShrinkResources = false
-      signingConfig = signingConfigs.getByName("debug")
-    }
-    if (keystoreProperties["keystoreFile"] != null) {
-      release {
-        isMinifyEnabled = false
-        isShrinkResources = false
-        isDebuggable = false
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro",
+        // API Keys
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        // Google Drive Keys
+        buildConfigField(
+            "String",
+            "DRIVE_CLIENT_ID",
+            System.getenv("DRIVE_CLIENT_ID")
+                ?: properties.getProperty("DRIVE_CLIENT_ID")
+                ?: "\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\""
         )
-        signingConfig = signingConfigs.getByName("release")
-      }
+        buildConfigField(
+            "String",
+            "DRIVE_API_KEY",
+            System.getenv("DRIVE_API_KEY")
+                ?: properties.getProperty("DRIVE_API_KEY")
+                ?: "\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\""
+        )
+        buildConfigField(
+            "String",
+            "DRIVE_APP_ID",
+            System.getenv("DRIVE_APP_ID")
+                ?: properties.getProperty("DRIVE_APP_ID")
+                ?: "\"XXXXXXXXXXXX\""
+        )
+        // Datadog Keys
+        buildConfigField(
+            "String",
+            "DATADOG_CLIENT_TOKEN",
+            System.getenv("DATADOG_CLIENT_TOKEN")
+                ?: properties.getProperty("DATADOG_CLIENT_TOKEN")
+                ?: "\"XXXXXXXXXXXX\""
+        )
+        buildConfigField(
+            "String",
+            "DATADOG_APPLICATION_ID",
+            System.getenv("DATADOG_APPLICATION_ID")
+                ?: properties.getProperty("DATADOG_APPLICATION_ID")
+                ?: "\"XXXXXXXXXXXX\""
+        )
     }
-  }
-  kotlinOptions { jvmTarget = "1.8" }
-  buildFeatures {
-    viewBinding = true
-    compose = true
-    buildConfig = true
-  }
-  composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
-  packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  // Allow references to generated code (Hilt)
-  kapt { correctErrorTypes = true }
+
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+            storeFile = file("../.debug/debug.jks")
+            storePassword = "android"
+        }
+        if (keystoreProperties["keystoreFile"] != null) {
+            create("release") {
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+                storeFile = file(keystoreProperties.getProperty("keystoreFile"))
+                storePassword = keystoreProperties.getProperty("keystorePassword")
+            }
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        if (keystoreProperties["keystoreFile"] != null) {
+            release {
+                isMinifyEnabled = false
+                isShrinkResources = false
+                isDebuggable = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro",
+                )
+                signingConfig = signingConfigs.getByName("release")
+            }
+        }
+    }
+    kotlinOptions { jvmTarget = "1.8" }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+        buildConfig = true
+    }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    // Allow references to generated code (Hilt)
+    kapt { correctErrorTypes = true }
 }
 
 dependencies {
@@ -135,12 +157,12 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("io.coil-kt:coil-video:2.7.0")
 
-  implementation("com.github.bumptech.glide:glide:4.16.0")
-  kapt("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
 
     // ExoPlayer
-  implementation("androidx.media3:media3-exoplayer:1.4.0")
-  implementation("androidx.media3:media3-ui:1.4.0")
+    implementation("androidx.media3:media3-exoplayer:1.4.0")
+    implementation("androidx.media3:media3-ui:1.4.0")
 
     // Dependency injection
     implementation("com.google.dagger:hilt-android:2.51")
@@ -176,19 +198,18 @@ dependencies {
     implementation("com.datadoghq:dd-sdk-android-logs:2.14.0")
 }
 
-val projectDir = layout.projectDirectory
-val sceneDirectory = projectDir.dir("src/main/assets/scenes")
+val sceneProjectPath = "app/src/main/assets/scenes"
 
 spatial {
-  allowUsageDataCollection.set(true)
-  scenes {
-    // if you have installed Meta Spatial Editor somewhere else, update the file path.
-    // cliPath.set("/Applications/Meta Spatial Editor.app/Contents/MacOS/CLI")
-    exportItems {
-      item {
-        projectPath.set(sceneDirectory.file("Main.metaspatial"))
-        outputPath.set(sceneDirectory)
-      }
+    allowUsageDataCollection.set(true)
+    scenes {
+        // if you have installed Meta Spatial Editor somewhere else, update the file path.
+        // cliPath.set("/Applications/Meta Spatial Editor.app/Contents/MacOS/CLI")
+        exportItems {
+            item {
+                projectPath.set(File("$sceneProjectPath/Main.metaspatial"))
+                outputPath.set(File("app/src/main/assets/scenes"))
+            }
+        }
     }
-  }
 }
