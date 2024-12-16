009f8ebb7429cacc105a01118e9385aebffbd38a
Alqueraf <alqueraf@gmail.com>
2024-12-16 16:54:09 +0100

    feat(media_view): update panorama aspect ratio to 2.75 to match iOS logic

--- Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/data/gallery/model/MediaType.kt
+++ Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/data/gallery/model/MediaType.kt
@@ -22,7 +22,7 @@ enum class MediaType {
     const val raybanVideoHeight = 2064
 
     // Panorama
-    const val panoramaAspectRatioMin = 3.0f
+    const val panoramaAspectRatioMin = 2.75f
 
     // 360
     const val media360ExactAspectRatio = 2f
