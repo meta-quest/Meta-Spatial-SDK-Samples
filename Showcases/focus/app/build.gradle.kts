e1ed75b883d9568e8eeb9bdd4250c91e847322f6
anainespereyra <anainespereyra@gmail.com>
2025-01-23 13:08:16 -0800

    Focus Update Spatial SDK to v0.5.4 (#30)
    Summary: Pull Request resolved: https://github.com/meta-quest/Meta-Spatial-SDK-Samples/pull/30
    
    Reviewed By: dav-s
    
    Differential Revision: D68573359
    
    fbshipit-source-id: 1a18a2261cdf86f6f1b4f90e03952b2f6247f6ff

--- Showcases/focus/app/build.gradle.kts
+++ Showcases/focus/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     minSdk = 28
     //noinspection ExpiredTargetSdkVersion
     targetSdk = 32
-    versionCode = 16
+    versionCode = 17
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
