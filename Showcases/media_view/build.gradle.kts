d914b22747403c640e5f28226082a1552d6726f5
Javier Torrus <jtorrusgalindo@gmail.com>
2025-02-25 15:02:03 -0800

    MediaView: Refactor LookAtHead component with new XML config (#33)
    Summary:
    * chore(media_view): refactor lookAtHead component with the new XML config
    
    * fix(media_view): move lookAtHead xml component to its corresponding main dir and bump spatial sdk version
    
    * chore(media_view): replace all references of the old component class with the new generated one
    
    * fix(media_view): complete v0.5.5 migration for LookAtHead component
    
    * refactor(media_view): remove unused ids file
    
    * build(media_view): increase app version
    
    ---------
    
    Pull Request resolved: https://github.com/meta-quest/Meta-Spatial-SDK-Samples/pull/33
    
    Reviewed By: pmcgvr
    
    Differential Revision: D70104241
    
    fbshipit-source-id: bd575abd082253980046ef7918bb5e928cdc8b6c

--- Showcases/media_view/build.gradle.kts
+++ Showcases/media_view/build.gradle.kts
@@ -2,7 +2,7 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  id("com.android.application") version "8.1.4" apply false
+  id("com.android.application") version "8.6.1" apply false
   id("org.jetbrains.kotlin.android") version "1.9.25" apply false
   id("com.google.dagger.hilt.android") version "2.51" apply false
   id("com.datadoghq.dd-sdk-android-gradle-plugin") version "1.14.0" apply false
