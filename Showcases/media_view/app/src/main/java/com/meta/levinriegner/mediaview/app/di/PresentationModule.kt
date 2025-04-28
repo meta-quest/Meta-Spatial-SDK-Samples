66dca7716c37233ae4a5822fcc18d8e932184bb3
Ivan Leider <ivan@levinriegner.com>
2025-04-28 10:52:13 -0400

    remove singleton

--- Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/app/di/PresentationModule.kt
+++ Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/app/di/PresentationModule.kt
@@ -18,8 +18,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object PresentationModule {
-
-  @Singleton
+  
   @Provides
   fun providePanelDelegate(): PanelDelegate {
     return SpatialActivityManager.getAppSystemActivity() as PanelDelegate
