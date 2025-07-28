32142efccece770221532f43b31b18219105055b
dirw-a11 <220655622+dirw-a11@users.noreply.github.com>
2025-07-29 07:13:41 +0800

    Delete SpatialVideoSample directory

--- SpatialVideoSample/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.jetbrains.kotlin.android) apply false
-}
