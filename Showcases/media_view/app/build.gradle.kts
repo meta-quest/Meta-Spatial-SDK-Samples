0f0e27091448e4984f6c43a5cf2e5e8b5a396180
Gautier de Lataillade <gautier2406@gmail.com>
2025-08-25 13:11:45 +0200

    Bump versionCode and versionName

--- Showcases/media_view/app/build.gradle.kts
+++ Showcases/media_view/app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
     minSdk = 29
     //noinspection ExpiredTargetSdkVersion
     targetSdk = 32
-    versionCode = 22
-    versionName = "0.0.20"
+    versionCode = 23
+    versionName = "0.0.21"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
